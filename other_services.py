c7dc438091a40d3cdca231a98e28b5ed5f4b619b
rayane.hammoumi@ens.uvsq.fr <Hammoumi>
2023-10-25 21:52:52 +0200

    minor changes

--- other_services.py
+++ other_services.py
@@ -9,11 +9,13 @@ from spyne.util.wsgi_wrapper import run_twisted
 
 from property_evaluation_service.property_evaluation_service import PropertyEvaluationService
 from Text_Extraction_Service import TextExtractionService
+from solvency_verification_service.solvency_verification_service import SolvencyVerificationService
+from credit_score_service.credit_score_service import calculate_credit_score
 
         
 
 # Créez une application Spyne pour coordonner les services
-application = Application([TextExtractionService, PropertyEvaluationService],
+application = Application([TextExtractionService, PropertyEvaluationService, calculate_credit_score, SolvencyVerificationService],
     tns='http://localhost/other-service',
     in_protocol=Soap11(validator='lxml'),
     out_protocol=Soap11()
