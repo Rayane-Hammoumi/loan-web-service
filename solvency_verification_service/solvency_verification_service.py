e79ec4c6aabc0191ceca7fb1e3f6462664b8b752
rayane.hammoumi@ens.uvsq.fr <Hammoumi>
2023-10-25 22:10:56 +0200

    correction d'erreur dans solvency verification

--- solvency_verification_service/solvency_verification_service.py
+++ solvency_verification_service/solvency_verification_service.py
@@ -1,26 +1,33 @@
 import logging
-logging.basicConfig(level=logging.DEBUG)
-import sys
-from spyne import Application, rpc, ServiceBase, \
-    Integer, Unicode
-from spyne import Iterable
+from spyne import Application, rpc, ServiceBase, Unicode
 from spyne.protocol.soap import Soap11
 from spyne.server.wsgi import WsgiApplication
 from spyne.util.wsgi_wrapper import run_twisted
 
+logging.basicConfig(level=logging.DEBUG)
 
 class SolvencyVerificationService(ServiceBase):
-    @rpc(Unicode, Unicode, _returns=Unicode)
+    @rpc(Unicode, Unicode, Unicode, Unicode, Unicode, _returns=Unicode)
     def verify_solvency(ctx, credit_score, loan_amount, loan_duration, monthly_income, monthly_expenses):
-        loan_duration_in_months=loan_duration*12
-        is_solvable = 1
-        not_solvable = 0
-        repayment_per_month = loan_amount / loan_duration / 12
+        loan_duration_in_months = int(loan_duration) * 12
+        is_solvable = "1"
+        not_solvable = "0"
+        repayment_per_month = float(loan_amount) / int(loan_duration) / 12
 
-        if credit_score == 0 :
+        if credit_score == "0":
             return not_solvable
-        elif (monthly_income - monthly_expenses - repayment_per_month) * loan_duration_in_months < 0:
-            #TODO: ajouter le solde du compte dans la bdd et ici
+        elif (int(monthly_income) - int(monthly_expenses) - repayment_per_month) * loan_duration_in_months < 0:
             return not_solvable
         else:
             return is_solvable
+
+application = Application([SolvencyVerificationService], 'your_namespace',
+                          in_protocol=Soap11(validator='lxml'),
+                          out_protocol=Soap11())
+
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+    wsgi_application = WsgiApplication(application)
+
+    server = make_server('0.0.0.0', 8000, wsgi_application)
+    server.serve_forever()
